f57ed1190e92d0c45bd3210099a2927b0957cfe6
David Oparanti <david.oparanti@decagon.dev>
2020-07-28 19:25:28 +0100

    Add applicant contructor

--- src/Applicants.java
+++ src/Applicants.java
@@ -1,7 +1,17 @@
 public class Applicants extends Persion {
+    private String phoneNo;
+    private String address;
+    private String yearApply;
+    private String courseApply;
+    private String dateOfBirth;
 
-    public Applicants(int id, String firstName, String lastName) {
+    public Applicants(int id, String firstName, String lastName, String phoneNo, String address, String yearApply, String courseApply, String dateOfBirth) {
         super(id, firstName, lastName);
+        this.phoneNo = phoneNo;
+        this.address = address;
+        this.yearApply = yearApply;
+        this.courseApply = courseApply;
+        this.dateOfBirth = dateOfBirth;
     }
 
 }
