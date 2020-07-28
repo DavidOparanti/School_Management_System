3f5c41954a3b2986a9f3314af1f39e01bd56ed96
David Oparanti <david.oparanti@decagon.dev>
2020-07-28 18:48:36 +0100

    Add students methods

--- src/Students.java
+++ src/Students.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Students extends Persion {
     private int grad;
     private double schoolFee;
     private double feePaid;
     private double feeOwn;
+    private List course;
 
     public Students(int id, String firstName, String lastName, int grad, double schoolFee) {
         super(id, firstName, lastName);
@@ -13,5 +17,45 @@ public class Students extends Persion {
 
     public int getGrade() {
         return this.grad;
+    }
+
+    public int getGrad() {
+        return grad;
+    }
+
+    public void setGrad(int grad) {
+        this.grad = grad;
+    }
+
+    public double getSchoolFee() {
+        return schoolFee;
+    }
+
+    public void setSchoolFee(double schoolFee) {
+        this.schoolFee = schoolFee;
+    }
+
+    public double getFeePaid() {
+        return feePaid;
+    }
+
+    public void setFeePaid(double feePaid) {
+        this.feePaid = feePaid;
+    }
+
+    public double getFeeOwn() {
+        return this.schoolFee - this.feePaid;
+    }
+
+    public void setFeeOwn(double feeOwn) {
+        this.feeOwn = this.getFeeOwn();
+    }
+
+    public String getCourse() {
+        return course.toString();
+    }
+
+    public void setCourse(List course) {
+        this.course = new ArrayList<>();
     }
 }
