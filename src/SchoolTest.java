08f9246b66beffc18363da2488de35f6a09c2d7f
David Oparanti <david.oparanti@decagon.dev>
2020-07-29 14:00:53 +0100

    Test school methods

--- src/SchoolTest.java
+++ src/SchoolTest.java
@@ -1,6 +1,27 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class SchoolTest <principal> {
+
+    /**
+     * Defining global variable to use for test case;
+     */
+    Teachers teacher = new Teachers(1,"Lara", "George", 17_000.00);
+    Courses algorithm = new Courses(1, "CSC304", "Algorithm", 4);
+    Courses algebra = new Courses(2,"MTH101", "Algebra", 3);
+    Students student = new Students(1,"Benedict", "look", 4,15_000.00);
+
+
+    ArrayList<Students> studentsList = new ArrayList<Students>();
+    List<Staff> staffsList = new ArrayList<>();
+    List<Courses> courseList = new ArrayList<>();
+    List<Classes> classList = new ArrayList<>();
+
+
+
+
     @Test
     void getTeachers() {
     }
@@ -34,63 +55,6 @@ class SchoolTest <principal> {
     }
 
 
-//    /**
-//    * Global Variable
-//     *
-//     * classes used for the test
-//    */
-//
-//    Staff principal = new Principal(1,"Idowu","Malik Bello", 400.00);
-//    Staff teacher = new Teachers(3, "Benedict", "Opanachi", 300.00);
-//    Students student1 = new Students(1,"Isreal", "Olamilekan", 4, 2000.00);
-//    Students student2 = new Students(2,"Daniel", "Adeleke", 3,4000.00);
-//    Courses algorithm = new Courses(1, "CSC304", "Algorithm", 4);
-//    Courses algebra = new Courses(2,"MTH101", "Algebra", 3);
-//
-//
-//    /**
-//     * List of classes to poly fill test.
-//     */
-//
-//     List<Students> studentsList = new ArrayList<Students>();
-//     //System.out.println(studentsList.toString())
-//     List<Staff> staffsList = new ArrayList<>();
-//     List<Courses> courseList = new ArrayList<>();
-//     List<Classes> classList = new ArrayList<>();
-//
-//    /**
-//     * list of staff
-//     */
-//
-//
-//
-//
-//
-//    @Test
-//    public void getId(){
-//        assertEquals(3,teacher.getId());
-//    }
-//
-//    @Test
-//    public void getSalay() {
-//        assertEquals(500, teacher.getSalary());
-//    }
-//
-//    @Test
-//    public void fullName() {
-//        assertEquals("David Oparanti", teacher.getFullName());
-//    }
-//
-//    @Test
-//    public void paySalary() {
-//        assertEquals(5000, teacher.TotalMoneyEarn() + teacher.getSalary());
-//    }
-//
-//
-//
-//
-//
-//
 
 
 }
